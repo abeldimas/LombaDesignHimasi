eebdfe80fca6cdceac68078d75ce381c780c1bdb
Abel Dimas <abeldimas13@gmail.com>
2020-06-23 01:59:36 +0800

    Responsive Mobile
    Mainpage responsive mobile, but not for tablet :)

---
+++ assets/js/MainScript.js
@@ -0,0 +1,7 @@
+
+//Responsive Design
+var windowWidth = $(window).width();
+if(windowWidth < 768){
+    $('.respontext1').insertBefore('.respontext');
+    $('.upacara2-2').insertBefore('.upacara2-1');
+}
