99d93c32aa6b7b2c8351759b14c3c44e9ed9ef2f
Abel Dimas <abeldimas13@gmail.com>
2020-06-23 16:44:13 +0800

    Full Responsive
    Full Responsive MainPage, No Animation

--- assets/js/MainScript.js
+++ assets/js/MainScript.js
@@ -1,7 +1,10 @@
-
 //Responsive Design
 var windowWidth = $(window).width();
-if(windowWidth < 768){
+console.log(windowWidth);
+if(windowWidth <= 768){
     $('.respontext1').insertBefore('.respontext');
+    $('.upacara2-2').insertBefore('.upacara2-1');
+}
+if(windowWidth < 1024){
     $('.upacara2-2').insertBefore('.upacara2-1');
 }
