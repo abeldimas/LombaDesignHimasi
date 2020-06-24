7291f57d275a089168b06babb8bb7820f0a8b41c
Abel Dimas <abeldimas13@gmail.com>
2020-06-25 03:07:38 +0800

    Responsive !!
    Perbaikan Index & Responsive All

--- assets/js/MainScript.js
+++ assets/js/MainScript.js
@@ -1,6 +1,5 @@
 //Responsive Design
 var windowWidth = $(window).width();
-console.log(windowWidth);
 if(windowWidth <= 768){
     $('.respontext1').insertBefore('.respontext');
     $('.upacara2-2').insertBefore('.upacara2-1');
