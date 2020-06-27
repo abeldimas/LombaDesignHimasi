9c2c91c00953d053796ef6f6fe8deeb6c084727d
Abel Dimas <abeldimas13@gmail.com>
2020-06-28 02:42:36 +0800

    Auto stash before merge of "master" and "origin/master"

--- assets/js/MainScript.js
+++ assets/js/MainScript.js
@@ -2,6 +2,7 @@
 var windowWidth = $(window).width();
 if(windowWidth <= 768){
     $('.respontext1').insertBefore('.respontext');
+    $('.box-upacara-1').insertBefore('.red-bordr');
     $('.upacara2-2').insertBefore('.upacara2-1');
 }
 if(windowWidth < 1024){
