acb8c73f1d32ab9b6bcc4cb1a8e85b15485e829f
Abel Dimas <abeldimas13@gmail.com>
2020-06-29 02:22:07 +0800

    Animation
    Tes Scroll Animation

--- assets/js/MainScript.js
+++ assets/js/MainScript.js
@@ -1,5 +1,17 @@
+$(document).ready(function(){
+
+    //Smooth Scrolling
+    let scrollButton = $('#smoothScroll');
+    scrollButton.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 2000)
+    })
+})
+
 //Responsive Design
-var windowWidth = $(window).width();
+let windowWidth = $(window).width();
 if(windowWidth <= 768){
     $('.respontext1').insertBefore('.respontext');
     $('.box-upacara-1').insertBefore('.red-bordr');
@@ -7,4 +19,10 @@ if(windowWidth <= 768){
 }
 if(windowWidth < 1024){
     $('.upacara2-2').insertBefore('.upacara2-1');
+}
+
+if(windowWidth > 1024){
+    //Fullpage Scroll
+    FastClick.attach(document.body);
+    $('#fullpage').fullpage();
 }
